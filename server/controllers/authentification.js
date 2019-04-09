5f5de2f1e8211e0fd04008e1118de62b1437ce90
Othman ASSAS <o.assas@gop.link>
2019-04-09 11:13:44 +0200

    authentification

--- server/controllers/authentification.js
+++ server/controllers/authentification.js
@@ -1,5 +1,28 @@
+const User = require('../models/user');
+const _ = require('lodash')
+
 exports.signup = (req, res, next) => {
     const { email, password } = req.body;
-    console.log(email, password);
-    res.send({ email, password });
+    
+    User.findOne({email}, (err, existingUser)=> {
+        if(err){
+            return next(err)
+        }
+        if(existingUser){
+            return res.status(422).send({error:"Email utilisé"})
+        }
+        if(_.isEmpty(email) || _.isEmpty(password)){
+            return res.status(422).send({error:"Email ou mot de passe vide"})
+        } else {
+            const user = new User({
+                email, password
+            })
+            user.save((err)=>{
+                if(err){
+                    return next(err)
+                }
+                res.json(user);
+            })
+        }
+    })
 };
