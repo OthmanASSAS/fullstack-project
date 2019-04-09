1d8f44d3a685ac65c804b7319ed64e5305a5d4b9
Othman ASSAS <o.assas@gop.link>
2019-04-09 12:58:24 +0200

    create token with jwt-simple

--- server/controllers/authentification.js
+++ server/controllers/authentification.js
@@ -1,28 +1,44 @@
-const User = require('../models/user');
-const _ = require('lodash')
+const User = require("../models/user");
+const _ = require("lodash");
+const jwt = require("jwt-simple");
+require("dotenv").config();
+
+const getTokenForUser = user => {
+    const timeStamp = new Date().getTime();
+    return jwt.encode(
+        {
+            sub: user.id,
+            iat: timeStamp
+        },
+        process.env.secret
+    );
+};
 
 exports.signup = (req, res, next) => {
     const { email, password } = req.body;
-    
-    User.findOne({email}, (err, existingUser) =>{
-        if(err){
-            return next(err)
+
+    User.findOne({ email }, (err, existingUser) => {
+        if (err) {
+            return next(err);
         }
-        if(existingUser){
-            return res.status(422).send({error:"Email utilisé"})
+        if (existingUser) {
+            return res.status(422).send({ error: "Email utilisé" });
         }
-        if(_.isEmpty(email) || _.isEmpty(password)){
-            return res.status(422).send({error:"Email ou mot de passe vide"})
+        if (_.isEmpty(email) || _.isEmpty(password)) {
+            return res
+                .status(422)
+                .send({ error: "Email ou mot de passe vide" });
         } else {
             const user = new User({
-                email, password
-            })
-            user.save((err)=>{
-                if(err){
-                    return next(err)
+                email,
+                password
+            });
+            user.save(err => {
+                if (err) {
+                    return next(err);
                 }
-                res.json(user);
-            })
+                res.json({ token: getTokenForUser(user) });
+            });
         }
-    })
+    });
 };
